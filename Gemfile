371c941458b80aa760373e3aaa5da79d38d5e747
Kazuhito Hokamura <kazuhito-hokamura@cookpad.com>
2016-02-15 18:20:37 +0900

    Update env

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
-ruby '2.0.0'
+ruby '2.3.0'
 
 gem 'middleman'
 gem 'middleman-blog'
 gem 'middleman-syntax'
+gem 'middleman-sprockets', '~> 4.0.0.rc'
 gem 'redcarpet'
 gem "builder"
--- config.rb
+++ config.rb
@@ -7,22 +7,21 @@ activate :syntax
 
 activate :blog do |blog|
   blog.layout = "_layouts/post"
-  blog.permalink = "archive/:year/:month/:title.html"
-  blog.sources = "archive/:year-:month-:day-:title.html"
+  blog.permalink = "archive/{year}/{month}/{title}.html"
+  blog.sources = "archive/{year}-{month}-{day}-{title}.html"
   blog.default_extension = ".md"
 
   blog.taglink = "tags/:tag.html"
   blog.tag_template = "tag.html"
 end
 
-page '/atom.xml', :layout => false
+page '/atom.xml', layout: false
 
 set :layouts_dir, '_layouts'
-set :partials_dir, '_partials'
 set :css_dir, 'css'
 set :js_dir, 'js'
 set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true
+set :markdown, fenced_code_blocks: true
 
 configure :development do
   sprockets.cache = ::Sprockets::Cache::FileStore.new("#{root}/.assets_cache")
